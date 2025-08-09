93f9437c584b373b6f247493d418cd9dc423f644
Vinicius Yahagi Rios <viniciusyahagirios@hotmail.com>
2025-08-09 09:30:11 +0900

    vercel

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,6 @@
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+injectSpeedInsights();
+
 export const ssr = false;
-export const prerender = true
+export const prerender = true;
